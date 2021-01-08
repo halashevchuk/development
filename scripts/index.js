f334de33dc3ed3db7fc9af741b6a0c94941b1bbd
halyna.shevch <halyna.shevch@gmail.com>
2021-01-08 12:33:58 +0200

    version3

--- scripts/index.js
+++ scripts/index.js
@@ -8,13 +8,13 @@ async function main() {
 
 
 
-     for (var i = 0; i < json.length; i++) {
-            let img = document.createElement('img');
-           // img.style = 'position:fixed;top:10px;left:10px;width:100px';
-            document.body.append(img);
-            img.src = json[i].url
-            }
+    for (const item of json) {
 
+        let img = document.createElement('img');
+       // img.style = 'position:fixed;top:10px;left:10px;width:100px';
+        document.body.append(img);
+        img.src = item.url
+      }
   
 }
 main();
