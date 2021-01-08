5bb5b1a066c4717c88abb33492f49d10112799a4
halyna.shevch <halyna.shevch@gmail.com>
2021-01-08 14:46:41 +0200

    version6

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,16 @@
 async function main() {
 
 
-    let response = await fetch('http://jsonplaceholder.typicode.com/photos');
+  let response = await fetch('http://jsonplaceholder.typicode.com/photos');
 
-    let json = await response.json()
+  let json = await response.json()
 
-    var text = "";
-    var i = 0;
-    do {
+  for (const item of json) {
 
-        i++;
-        let img = document.createElement('img');
-        //img.style = 'position:fixed;top:10px;left:10px;width:100px';
-        document.body.append(img);
-        img.src = json[i].url
-    }
-    while (i < json.length);
+    let img = document.createElement('img');
+    // img.style = 'position:fixed;top:10px;left:10px;width:100px';
+    document.body.append(img);
+    img.src = item.url
+  }
 }
 main();
