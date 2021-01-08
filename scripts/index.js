a282de0765a0a58d7ea661ac01098fda99583903
halyna.shevch <halyna.shevch@gmail.com>
2021-01-08 14:34:24 +0200

    initial version

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+async function main() {
+
+
+    let response = await fetch('http://jsonplaceholder.typicode.com/photos');
+
+    let json = await response.json()
+
+
+
+  for (var i = 0; i < json.length; i++) {
+        let img = document.createElement('img');
+       // img.style = 'position:fixed;top:10px;left:10px;width:100px';
+        document.body.append(img);
+        img.src = json[i].url
+        }
+
+}
+main();
