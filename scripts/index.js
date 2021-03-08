0726b1294b067b3f0eef2eef3aa4f2c21efca8d8
halyna.shevch <halyna.shevch@gmail.com>
2021-03-08 14:33:48 +0200

    last changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,72 @@
-async function main() {
+// async function main() {
 
 
-    let response = await fetch('http://jsonplaceholder.typicode.com/photos');
+// let response = await fetch('http://jsonplaceholder.typicode.com/photos');
 
-    let json = await response.json()
-
-
+// let json = await response.json()
 
 
-    var text = "";
-    var i = 0;
-    do {
 
-        i++;
-        let img = document.createElement('img');
-        //img.style = 'position:fixed;top:10px;left:10px;width:100px';
-        document.body.append(img);
-        img.src = json[i].url
+
+//     json.forEach((item, index) => { 
+//     if (index<100){
+//     let img = document.createElement('img');
+//     //img.style = 'position:fixed;top:10px;left:10px;width:100px';
+//     document.body.append(img);
+//     img.src = item.url
+//     }
+// })
+
+
+// }
+var rating = document.querySelector('.rating')
+var ratingItem = document.querySelectorAll('.rating .rating-item');
+var activeItems = document.querySelectorAll('.rating .rating-item.active').length;
+
+
+
+var cStars = function (nowPos) {
+
+    for (var i = 0; ratingItem.length > i; i++) {
+        ratingItem[i].classList.remove('active');
     }
-    while (i < json.length);
-
+    for (var i = 0; nowPos + 1 > i; i++) {
+        ratingItem[i].classList.toggle('active');
+    }
 }
-main();
+
+
+rating.addEventListener('mouseover', function (e) {
+    var myTarget = e.target;
+    var i = ratingItem.length;
+    while (i--) {
+        if (ratingItem[i] == myTarget) {
+            var currentIndex = i;
+            break;
+        }
+    }
+    cStars(currentIndex);
+});
+
+
+
+rating.addEventListener('click', function(e) {
+	var myTarget = e.target;
+  var i = ratingItem.length;
+  while(i--) {
+    if(ratingItem[i] == myTarget) {
+      var currentIndex = i;
+      break;
+    }
+  }
+    cStars(currentIndex);
+    activeItems = document.querySelectorAll('.rating .rating-item.active').length;
+});
+
+
+rating.addEventListener('mouseleave', function (e) {
+    cStars(activeItems - 1);
+});
+
+
+
